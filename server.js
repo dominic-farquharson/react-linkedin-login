0b0d0901216453267a1f23d5646b07d3e2b14115
Dominic Farquharson <farquharsondominic@gmail.com>
2018-02-05 23:09:06 -0500

    retrieve code and state from redirect uri

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('home')
+// config
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+// hard coding state for now
+const genState = '987654321';
+
+// add middleware to check state
+app.get('/auth/linkedin/callback', (req, res) => {
+  console.log(req.query);
+  if(req.query.state !== genState) {
+    return res.status(401).send('not authorized');
+  }
+
+  const { code, state } = req.query;
+
+  res.json({
+    code,
+    state
+  });
 })
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`server listning on PORT: ${PORT}`)
-})
+  console.log(`server listning on PORT: ${PORT}`);
+});
